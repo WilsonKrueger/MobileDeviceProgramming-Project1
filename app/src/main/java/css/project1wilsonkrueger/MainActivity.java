fb11ee61e04ae236cd833b71452130a64d25dfd3
Wilson Krueger <wkrueger@css.edu>
2021-03-01 14:52:55 -0600

    Initial brainstorming creation. Added DatePickerFragment class for the buttonCalendar

--- app/src/main/java/css/project1wilsonkrueger/MainActivity.java
+++ app/src/main/java/css/project1wilsonkrueger/MainActivity.java
@@ -1,14 +1,37 @@
 package css.project1wilsonkrueger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonCalendar;
+    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        setupButtonCalendar();
+    }
+
+    private void setupButtonCalendar() {
+        buttonCalendar = findViewById(R.id.buttonCalendar);
+        buttonCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePickerDialog();
+            }
+        });
+    }
+
+    public void showDatePickerDialog() {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
